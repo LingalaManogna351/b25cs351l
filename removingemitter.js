20e4a9779c0592563e5c7d4e00c8a0eec90d005e
LingalaManogna351 <b25cs351l@kitsw.ac.in>
2026-08-12 15:57:47 +0530

    Add files via upload

---
+++ removingemitter.js
@@ -0,0 +1,9 @@
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+function welcome() {
+ console.log("welcome");
+}
+eventEmitter.on("login",welcome);
+eventEmitter.emit("login");
+eventEmitter.removeListener("login",welcome);
+eventEmitter.emit("login");
